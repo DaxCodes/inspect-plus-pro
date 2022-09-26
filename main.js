92fb236a2916ac76569025954b834d6be53f3480
Daxton B <93626895+DaRedDeveloper@users.noreply.github.com>
2022-09-25 18:55:58 -0600

    Replaced with a loader
    easier

---
+++ main.js
@@ -0,0 +1,10 @@
+function load() {
+    var loader = document.createElement('script');
+    loader.setAttribute('src', 'https://inspect-pro-source.daxcodes.repl.co/script.js');
+    loader.setAttribute('type', 'text/javascript');
+    document.appendChild(loader);
+}
+
+load()
+
+// Bookmarklet Script: javascript:var loader = document.createElement('script'); loader.src = 'https://inspect-pro-source.daxcodes.repl.co/script.js'; document.head.appendChild(loader); void 0
